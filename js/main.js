40df7582bf38205373b62c24da603310f8d1e8e3
yassan83830 <o830takumay@gmail.com>
2022-10-29 18:16:24 +0900

    tutorial vuejs.org

--- js/main.js
+++ js/main.js
@@ -3,32 +3,56 @@
 
     Vue.config.devtools = true;
     
-    var vm = new Vue({
+    var app = new Vue({
         el: '#app',
         data: {
-            newComment: '',
-            todos: [
-                {comment: "a", isDone: false},
-                {comment: "b", isDone: true},
-                {comment: "c", isDone: false}
-            ]
-        },
-        methods:{
-            addTodo: function(){
-                if(this.newComment == '')
-                    return;
-
-                let todo = {
-                    comment: this.newComment,
-                    isDone: false
-                };
-                this.todos.push(todo);
-                this.newComment = '';
-            },
-            deleteComment: function(index){
-                this.todos.splice(index,1)
-            }
+            message: 'Hello Vue!'
         }
     });
+
+    var app2 = new Vue({
+        el: '#app-2',
+        data: {
+            message: "You loaded this page on " + new Date().toLocaleDateString()
+        }
+    })
+
+    var app3 = new Vue({
+        el: '#app-3',
+        data: {
+            seen: true
+        }
+    })
+
+    var app4 = new Vue({
+        el: '#app-4',
+        data: {
+            todos: [
+                { text: 'Learn JavaScript'},
+                { text: 'Learn Vue'},
+                { text: 'Build somothing awesome'}
+            ]
+        }
+    })
+
+    var app5 = new Vue({
+        el: '#app-5',
+        data: {
+            message: 'Hello Vue.js!'
+        },
+        methods: {
+            reverseMessage: function () {
+                this.message = this.message.split('').reverse().join('')
+            }
+        }
+    })
+
+    var app6 = new Vue({
+        el: '#app-6',
+        data: {
+            message: 'Hello Vue!'
+        }
+    })
+
 
 })();
