aee33b727774c09f77909f82cbbc02b20d66e12c
yassan83830 <o830takumay@gmail.com>
2022-10-30 21:23:00 +0900

    add header menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,28 @@
 
     Vue.config.devtools = true;
     
+    var menu = new Vue({
+        el: header,
+        data(){
+            return {
+                menus: [
+                    {
+                        label: "TOP",
+                        path: "./index.html"
+                    },
+                    {
+                        label: "PROFILE",
+                        path: "./index.html"
+                    },
+                    {
+                        label: "WORKS",
+                        path: "./index.html"
+                    }
+                ]
+            };
+        }
+    })
+
     var img_bg = new Vue({
         el: '#img_bg',
         data: {
