26ece28ca026a8e9d7b87b290a9d132c609e91f5
yassan83830 <o830takumay@gmail.com>
2022-11-13 20:06:18 +0900

    [Fix] data move -> app

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    Vue.config.devtools = true;
-    
-    new Vue({
-        el: '#app',
-        vuetify: new Vuetify(),
-      })
+  Vue.config.devtools = true;
 
-    // var background = new Vue({
-    //     el: '.background',
-    //     data: {
-    //         footer: "@ 2022- yassan83830"
-    //     }
-    // })
+  var app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+      items: [
+        { title: 'Top' },
+        { title: 'Profile' },
+        { title: 'Works' }
+      ],
+      edu_title: 'Education',
+      edus: [
+        { year: '2014-2019', school: '広島商船高等専門学校 流通情報工学科 卒業' },
+        { year: '2018 August(2weeks)', school: 'Emilio Aguinaldo College International Language Institute Manila, Philippines English Proficiency Program 修了' },
+        { year: '2019-2021', school: '豊橋技術科学大学 工学部 情報・知能工学科 卒業' },
+        { year: '2021-', school: '豊橋技術科学大学 博士前期課程 情報・知能工学専攻 在学中' },
+      ]
+    }
+  })
 
-})();
+
+}) ();
